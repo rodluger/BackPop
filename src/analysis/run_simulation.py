fb054150f3019e4071ffd00e48e75b4a096a4a1d
Kaze Wong <kazewong.physics@gmail.com>
2022-02-07 12:32:35 -0500

    Initialize zenodo-tarball

---
+++ src/analysis/run_simulation.py
@@ -0,0 +1,15 @@
+"""
+Run a (fake) simulation and generate a simulation results file.
+
+"""
+import numpy as np
+
+for value in range(10):
+
+    # Generate an array of random numbers with a given seed.
+    # This is a placeholder for an "expensive" operation.
+    np.random.seed(value)
+    X = np.random.randn(10)
+
+    # Save the "simulation" results to disk
+    np.savetxt(f"../data/results_{value}.dat", X=X)
